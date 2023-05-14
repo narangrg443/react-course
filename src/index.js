4d7026b830941f384ce5db520ce3af61d2ecaa34
narangrg443 <narangrg48@gmail.com>
2023-05-14 20:54:12 +0545

    creating by component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from "react"
 import ReactDom from "react-dom"
 
+let c = {
+  color: ""
+}
 
+const t = 1;
+if (t === 12) {
+  c.color = "red"
+} else {
+  c.color = "blue"
+}
+
+function By() {
+  return <b>byyyyy</b>
+}
 
 
 const name = 'mandep';
 ReactDom.render(
   <div>
-    <h1>hello{name}</h1>
+    <h1 style={c}>hello{name}</h1>
      <ul>
      <li>apple</li>
   </ul>
+  <By />
   </div>,
   document.getElementById('root'));
