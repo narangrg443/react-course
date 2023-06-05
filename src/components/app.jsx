ae5b289f943142b08c75e95d5a284d7949e4a92e
narangrg443 <narangrg48@gmail.com>
2023-06-05 09:13:13 +0545

    to do list

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,52 +1,29 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: ""
-  });
+  const [todoList, setTodoList] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
 
-  const changeHandlerf = (e) => {
-    setContact({ ...contact, fName: e.target.value });
-  };
-
-  const changeHandlerl = (e) => {
-    setContact({ ...contact, lName: e.target.value });
-  };
-
-  const changeHandlere = (e) => {
-    setContact({ ...contact, email: e.target.value });
+  const addTodo = () => {
+    setTodoList([...todoList, newTodo]);
+    setNewTodo("");
   };
 
   return (
     <div className="app-container">
-      <div>
-      <h1>
-        Hello {contact.fName} {contact.lName}
-      </h1>
-      <p>{contact.email}</p>
-       </div>
-      <form>
-        <input
-          name="fName"
-          onChange={changeHandlerf}
-          placeholder="First Name"
-        />
-        <input
-          name="lName"
-          onChange={changeHandlerl}
-          placeholder="Last Name"
-        />
-        <input
-          name="email"
-          onChange={changeHandlere}
-          placeholder="Email"
-        />
-<div className="btn-submit">
-        <button>Submit</button>
-</div>
-      </form>
+      <h2>Todo List</h2>
+      <input
+        type="text"
+        placeholder="Add to-do item"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+      <div className="todo-list">
+        {todoList.map((todo, index) => (
+          <div key={index}>{todo}</div>
+        ))}
+      </div>
     </div>
   );
 };
